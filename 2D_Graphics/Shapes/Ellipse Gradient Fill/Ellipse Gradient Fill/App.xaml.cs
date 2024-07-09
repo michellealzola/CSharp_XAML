e2f982315f0d692b527ec6bb61480ba78b6616eb
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-08 22:35:41 -0600

    Ellipse Gradient Fill

---
+++ 2D_Graphics/Shapes/Ellipse Gradient Fill/Ellipse Gradient Fill/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ellipse_Gradient_Fill
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 2D_Graphics/Shapes/Ellipse Gradient Fill/Ellipse Gradient Fill/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ellipse_Gradient_Fill
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GradientStop gradientsStop1;
+        private GradientStop gradientsStop2;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gradientsStop1 = new GradientStop(Colors.CadetBlue, 0);
+            gradientsStop2 = new GradientStop(Colors.Coral, 1);
+
+            LinearGradientBrush gradientBrush = new LinearGradientBrush();
+            gradientBrush.GradientStops.Add(gradientsStop1);
+            gradientBrush.GradientStops.Add(gradientsStop2);
+
+            GradientEllipse.Fill = gradientBrush;
+        }
+
+        private void ColorPicker1_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if(e.NewValue.HasValue)
+            {
+                gradientsStop1.Color = e.NewValue.Value;
+            }
+        }
+
+        private void OffsetSlider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            gradientsStop1.Offset = e.NewValue;
+        }
+
+        private void ColorPicker2_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if(e.NewValue.HasValue)
+            {
+                gradientsStop2.Color = e.NewValue.Value;
+            }
+        }
+
+        private void OffsetSlider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            gradientsStop2.Offset = e.NewValue;
+        }
+    }
+}
