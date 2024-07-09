c3ec716d18e61e52ca476e414bdd8735c4f5a826
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-08 19:51:54 -0600

    Rectangle Fill and Stroke

---
+++ 2D_Graphics/Shapes/RectangleFillAndStroke/RectangleFillAndStroke/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RectangleFillAndStroke
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Fill_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if(e.NewValue.HasValue)
+            {
+                ColorRectangle.Fill = new SolidColorBrush(e.NewValue.Value);
+            }
+        }
+
+        private void Stroke_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if(e.NewValue.HasValue)
+            {
+                ColorRectangle.Stroke = new SolidColorBrush(e.NewValue.Value);
+            }
+        }
+    }
+}
