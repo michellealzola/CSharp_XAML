b5a44216469705e78015f140b56083e940be87cd
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-08 18:49:22 -0600

    Dynamic Resizing Rectangle

---
+++ 2D_Graphics/Shapes/DynamicResizingRectangle/DynamicResizingRectangle/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DynamicResizingRectangle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WidthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(ResizableRectangle != null)
+            {
+                ResizableRectangle.Width = e.NewValue;
+            }
+        }
+
+        private void HeightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (ResizableRectangle != null)
+            {
+                ResizableRectangle.Height = e.NewValue;
+            }
+        }
+
+        private void CornerSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (ResizableRectangle != null)
+            {
+                ResizableRectangle.RadiusX = e.NewValue;
+                ResizableRectangle.RadiusY = e.NewValue;
+            }
+        }
+    }
+}
