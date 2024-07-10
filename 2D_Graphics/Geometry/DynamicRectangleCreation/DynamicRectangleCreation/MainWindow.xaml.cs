7d1f0c7e0d80ab5de6a8a370958a4f249f11023c
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-09 21:50:50 -0600

    Dynamic Rectangle Creation

---
+++ 2D_Graphics/Geometry/DynamicRectangleCreation/DynamicRectangleCreation/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DynamicRectangleCreation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawRectangleButton_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingCanvas.Children.Clear();
+
+            double width;
+            double height;
+            double xpos;
+            double ypos;
+
+            if (double.TryParse(WidthTextBox.Text, out width) &&
+                double.TryParse(HeightTextBox.Text,out height) &&
+                double.TryParse(XPositinTextBox.Text, out xpos) &&
+                double.TryParse(YPositinTextBox.Text, out ypos))
+            {
+                RectangleGeometry rectangleGeometry = new RectangleGeometry(new Rect(xpos, ypos, width, height));
+
+                Path rectanglePath = new Path
+                {
+                    Data = rectangleGeometry,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2,
+                    Fill = Brushes.Blue,
+
+                };
+
+                DrawingCanvas.Children.Add(rectanglePath);
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numeric values for width, height, X position, and Y position.");
+            }      
+
+            
+        }
+    }
+}
