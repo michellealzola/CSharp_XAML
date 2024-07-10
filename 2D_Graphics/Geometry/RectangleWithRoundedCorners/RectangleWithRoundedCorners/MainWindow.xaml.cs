0fb75bad6e446e526d7ece5f140ce3fe5785a48e
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-10 08:11:06 -0600

    Rectangle with Rounded Corners

---
+++ 2D_Graphics/Geometry/RectangleWithRoundedCorners/RectangleWithRoundedCorners/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RectangleWithRoundedCorners
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RectangleGeometry rect;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawRectangle();
+        }
+
+        private void DrawRectangle()
+        {
+            double width = 100;
+            double height = 100;
+            double xpos = 50;
+            double ypos = 50;
+
+            rect = new RectangleGeometry(new Rect(xpos, ypos, width, height));
+
+            Path rectPath = new Path
+            {
+                Data = rect,
+                Fill = Brushes.Brown,
+            };
+
+            DrawingCanvas.Children.Add(rectPath);
+        }
+
+        private void RadiusSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (rect == null)
+            {
+                return;
+            }
+
+            rect.RadiusX = e.NewValue;
+            rect.RadiusY = e.NewValue;  
+        }
+    }
+}
