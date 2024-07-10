af6e5df66c6c4efdc32c104e50487fdd17a9f180
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-10 11:57:27 -0600

    Rectangle Animation

---
+++ 2D_Graphics/Geometry/RectangleAnimation/RectangleAnimation/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RectangleAnimation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnimateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard storyboard = new Storyboard();
+
+            DoubleAnimation xAnimation = new DoubleAnimation
+            { 
+                From = 0,
+                To = 300,
+                Duration = new Duration(TimeSpan.FromSeconds(2)),
+            };
+
+            Storyboard.SetTarget(xAnimation, AnimatedRectangle);
+            Storyboard.SetTargetProperty(xAnimation, new PropertyPath("(Canvas.Left)"));
+
+            DoubleAnimation yAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = 200,
+                Duration = new Duration(TimeSpan.FromSeconds(2)),
+            };
+
+            Storyboard.SetTarget(yAnimation, AnimatedRectangle);
+            Storyboard.SetTargetProperty(yAnimation, new PropertyPath("(Canvas.Top)"));
+
+            storyboard.Children.Add(xAnimation);
+            storyboard.Children.Add(yAnimation);
+
+            storyboard.Begin();
+        }
+    }
+}
