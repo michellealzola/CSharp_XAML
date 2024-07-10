e57ab0f86fa2051456987820af3bb110567d8356
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-09 19:25:32 -0600

    Stroke Line Join

---
+++ 2D_Graphics/Strokes/StrokeLineJoin/StrokeLineJoin/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StrokeLineJoin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Rectangle rectangle;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawRectangle();
+        }
+
+        private void DrawRectangle()
+        {
+            rectangle = new Rectangle
+            {
+                Width = 200,
+                Height = 100,
+                Stroke = Brushes.Black,
+                StrokeThickness = 10,
+                StrokeLineJoin = PenLineJoin.Miter,
+                Margin = new Thickness(5)
+            };
+
+            DrawingCanvas.Children.Add(rectangle);
+
+        }
+
+        private void LineJoinComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (rectangle == null) 
+            {
+                return;
+            }
+
+            switch (LineJoinComboBox.SelectedIndex)
+            {
+                case 0:
+                    rectangle.StrokeLineJoin = PenLineJoin.Miter;
+                    break;
+                case 1:
+                    rectangle.StrokeLineJoin = PenLineJoin.Bevel;
+                    break;
+                case 2:
+                    rectangle.StrokeLineJoin = PenLineJoin.Round;
+                    break;
+            }
+        }
+    }
+}
