20f1a322520122e8d6c58c0837dd4371d632d5cb
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-11 17:43:02 -0600

    Basic PolyLine Segment

---
+++ 2D_Graphics/PathSegments/BAsic PolyLine Segment/BAsic PolyLine Segment/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BAsic_PolyLine_Segment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 2D_Graphics/PathSegments/BAsic PolyLine Segment/BAsic PolyLine Segment/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BAsic_PolyLine_Segment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddPolyLineSegment();
+        }
+
+        private void AddPolyLineSegment()
+        {
+            Path path = new Path
+            {
+                Stroke = Brushes.CadetBlue,
+                StrokeThickness = 2,
+            };
+
+            PathGeometry pathGeometry = new PathGeometry();
+
+            PathFigureCollection pathFigureCollection = new PathFigureCollection();
+
+            PathFigure pathFigure = new PathFigure
+            {
+                StartPoint = new Point(10, 50),
+            };
+
+            PathSegmentCollection pathSegmentCollection = new PathSegmentCollection();
+
+            PolyLineSegment polyLineSegment = new PolyLineSegment
+            {
+                Points = new PointCollection(new Point[] { new Point(200, 100), new Point(10, 100) }),
+                IsStroked = true,
+            };
+
+            pathSegmentCollection.Add(polyLineSegment);
+
+            pathFigure.Segments = pathSegmentCollection;
+
+            pathFigureCollection.Add(pathFigure);
+
+            pathGeometry.Figures = pathFigureCollection;
+
+            path.Data = pathGeometry;
+
+            DrawingCanvas.Children.Add(path);
+
+        }
+    }
+}
