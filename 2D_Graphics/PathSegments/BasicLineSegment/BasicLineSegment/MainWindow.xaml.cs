27100525418f74715d2a67cfbf8aabcc1fbaebf9
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-11 16:45:01 -0600

    Basic Line Segment

---
+++ 2D_Graphics/PathSegments/BasicLineSegment/BasicLineSegment/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicLineSegment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawingCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point mousePosition = e.GetPosition(DrawingCanvas);
+
+            lineSegment.Point = mousePosition;
+
+            PathGeometry pathGeometry = path.Data as PathGeometry;
+
+            if (pathGeometry != null && pathGeometry.Figures.Count > 0)
+            {
+                PathFigure pathFigure = pathGeometry.Figures[0];
+                pathFigure.StartPoint = new Point(100, 100);
+            }
+        }
+    }
+}
