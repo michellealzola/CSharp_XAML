ac9cd396aca0320194ea73d570c98825567b28af
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-11 18:25:38 -0600

    Basic Arc Segment

---
+++ 2D_Graphics/PathSegments/Basic Arc Segment/Basic Arc Segment/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Basic_Arc_Segment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 2D_Graphics/PathSegments/Basic Arc Segment/Basic Arc Segment/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Basic_Arc_Segment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateArcSegment();
+        }
+
+        private void CreateArcSegment()
+        {
+            Path path = new Path
+            {
+                Stroke = Brushes.Brown,
+                StrokeThickness = 2,
+            };
+
+            PathGeometry pathGeometry = new PathGeometry();
+
+            PathFigureCollection pathFigureCollection = new PathFigureCollection();
+
+            PathFigure pathFigure = new PathFigure
+            {
+                StartPoint = new Point(10, 50),
+            };
+
+            PathSegmentCollection pathSegmentCollection = new PathSegmentCollection();
+
+            ArcSegment arcSegment = new ArcSegment
+            {
+                Point = new Point(150, 150),
+                Size = new Size(50, 50),
+                RotationAngle = 45,
+                IsLargeArc = true,
+                SweepDirection = SweepDirection.Clockwise,
+                IsStroked = true,
+
+            };
+
+            pathSegmentCollection.Add(arcSegment);  
+
+            pathFigure.Segments = pathSegmentCollection;
+
+            pathFigureCollection.Add(pathFigure);
+
+            pathGeometry.Figures = pathFigureCollection;
+
+            path.Data = pathGeometry;
+
+            DrawingCanvas.Children.Add(path);
+
+        }
+
+    }
+}
