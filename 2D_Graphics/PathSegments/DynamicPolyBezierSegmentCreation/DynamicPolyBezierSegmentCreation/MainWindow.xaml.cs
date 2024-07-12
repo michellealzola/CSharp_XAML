36bc4be52d3059bc2172e5222998ebb3188fd8ea
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-12 11:22:32 -0600

    Dynamic PolyBezierSegment Creation

--- 2D_Graphics/PathSegments/DynamicPolyBezierSegmentCreation/DynamicPolyBezierSegmentCreation/MainWindow.xaml.cs
+++ 2D_Graphics/PathSegments/DynamicPolyBezierSegmentCreation/DynamicPolyBezierSegmentCreation/MainWindow.xaml.cs
@@ -16,9 +16,78 @@ namespace DynamicPolyBezierSegmentCreation
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PathFigure currentPathFigure;
+        private PathGeometry currentPathGeometry;
+        private PolyBezierSegment polyBezierSegment;
+        private bool isDrawing;
+        private Point lastPoint;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void DrawingCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point clickPoint = e.GetPosition(DrawingCanvas);
+            if (!isDrawing)
+            {
+                // Start a new path
+                Path path = new Path
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2
+                };
+
+                currentPathGeometry = new PathGeometry();
+                currentPathFigure = new PathFigure
+                {
+                    StartPoint = clickPoint
+                };
+
+                polyBezierSegment = new PolyBezierSegment();
+                polyBezierSegment.Points.Add(clickPoint);
+
+                currentPathFigure.Segments.Add(polyBezierSegment);
+                currentPathGeometry.Figures.Add(currentPathFigure);
+                path.Data = currentPathGeometry;
+
+                DrawingCanvas.Children.Add(path);
+                lastPoint = clickPoint;
+                isDrawing = true;
+            }
+            else
+            {
+                polyBezierSegment.Points.Add(clickPoint);
+                lastPoint = clickPoint;
+            }
+        }
+
+        private void DrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && polyBezierSegment.Points.Count > 0)
+            {
+                Point currentPoint = e.GetPosition(DrawingCanvas);
+
+                if (polyBezierSegment.Points.Count > 1)
+                {
+                    polyBezierSegment.Points[polyBezierSegment.Points.Count - 1] = currentPoint;
+                }
+
+                DrawingCanvas.InvalidateVisual();
+            }
+        }
+
+        private void DrawingCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Point releasePoint = e.GetPosition(DrawingCanvas);
+                polyBezierSegment.Points.Add(releasePoint);
+                lastPoint = releasePoint;
+                isDrawing = false;
+            }
         }
     }
 }
