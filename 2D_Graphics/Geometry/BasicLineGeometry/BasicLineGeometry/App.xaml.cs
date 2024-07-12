7da35ea96119194764c321c4821239319bba1957
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-12 17:39:49 -0600

    Basic LineGeometry

---
+++ 2D_Graphics/Geometry/BasicLineGeometry/BasicLineGeometry/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BasicLineGeometry
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 2D_Graphics/Geometry/BasicLineGeometry/BasicLineGeometry/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicLineGeometry
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateLineGeometry();
+        }
+
+        private void CreateLineGeometry()
+        {
+            double startPointX = 300;
+            double startPointY = 10;
+            double endPointX = 400;
+            double endPointY = 60;
+
+            Point startPointCoordinate = new Point(startPointX, startPointY);
+            Point endPointCoordinate = new Point(endPointX, endPointY);
+
+            LineGeometry lineGeometry = new LineGeometry(startPointCoordinate, endPointCoordinate);
+
+            Path path = new Path
+            {
+                Data = lineGeometry,
+                Stroke = Brushes.Brown,
+                StrokeThickness = 2,
+            };
+
+            DrawingCanvas.Children.Add(path);
+        }
+    }
+}
