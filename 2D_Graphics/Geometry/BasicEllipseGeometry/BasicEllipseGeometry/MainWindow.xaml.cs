ba7c140a01516ff7a7af020bf90b56708a3dd75a
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2024-07-12 17:24:40 -0600

    Basic EllipseGeometry

---
+++ 2D_Graphics/Geometry/BasicEllipseGeometry/BasicEllipseGeometry/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicEllipseGeometry
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateEllipseGeometry();
+        }
+
+        private void CreateEllipseGeometry()
+        {
+            double centerX = 200;
+            double centerY = 75;
+            double RadiusX = 50;
+            double RadiusY = 25;
+
+            EllipseGeometry ellipseGeometry = new EllipseGeometry(new Point(centerX, centerY), RadiusX, RadiusY);
+
+            Path path = new Path
+            {
+                Data = ellipseGeometry,
+                Fill = Brushes.Brown,
+            };
+
+            DrawingCanvas.Children.Add(path);
+        }
+    }
+}
